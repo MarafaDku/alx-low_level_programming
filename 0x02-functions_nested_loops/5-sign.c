9478c40b249f24398711cf11f0f85eb5095dfbfa
MarafaDku <usmanmarafad@gmail.com>
2022-09-13 05:52:25 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,26 +1,30 @@
-#include "holberton.h"
+#include"main.h"
 
 /**
- * print_sign - find if input is pos, neg, or 0
- * @n: Int to print sign from
+ * print_sign - print + if n is greater than zero,
+ *             0 if n is zero and - if n is less
+ *             than zero.
  *
- * Return: return 1 if pos, return 0 if 0, return -1 if neg
- */
+ * @n: takes integer type input for function.
+ *
+ * Return: 1 if +, 0 if 0 and -1 if -
+*/
+
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(43 + 0);
 		return (1);
 	}
-	else if (n < 0)
+	else if (n == 0)
 	{
-		_putchar('-');
-		return (-1);
+		_putchar(0 + 48);
+		return (0);
 	}
 	else
 	{
-		_putchar('0');
-		return (0);
+		_putchar(45 + 0);
+		return (-1);
 	}
 }
